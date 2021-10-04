724c817624d7f226d6a4d0f787672bae6f962f20
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-04 11:35:28 +0530

    Initial commit

---
+++ src/linkedList/SinglyLinkedList.java
@@ -0,0 +1,18 @@
+package linkedList;
+
+public class SinglyLinkedList {
+
+}
+
+class ListNode{
+    private int data;
+    private ListNode next;
+
+    public ListNode(int data) {
+        this.data = data;
+    }
+
+    public ListNode(ListNode next) {
+        this.next = next;
+    }
+}
