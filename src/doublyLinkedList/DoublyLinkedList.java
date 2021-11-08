50154476b20e474d4041908de8e66de71830a4bd
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-08 11:17:49 +0530

    define ListNode class

--- src/doublyLinkedList/DoublyLinkedList.java
+++ src/doublyLinkedList/DoublyLinkedList.java
@@ -1,4 +1,13 @@
 package doublyLinkedList;
 
 public class DoublyLinkedList {
+
+    class ListNode{
+        int data;
+        ListNode previous, next;
+        public ListNode(int data){
+            this.data = data;
+            this.previous = this.next = null;
+        }
+    }
 }
