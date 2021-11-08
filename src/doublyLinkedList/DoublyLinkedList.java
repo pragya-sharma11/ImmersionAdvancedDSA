214706184a03edaeb857679bc4c1fa3f84413110
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-08 11:29:19 +0530

    define method addFirst to add at first position

--- src/doublyLinkedList/DoublyLinkedList.java
+++ src/doublyLinkedList/DoublyLinkedList.java
@@ -5,7 +5,25 @@ public class DoublyLinkedList {
     //head and tail are the reference variable pointing to the start and
     //end of DoublyLinkedList
     private int size;
-
+    public DoublyLinkedList(){
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+    public boolean isEmpty(){
+        return size==0; //head == null;
+    }
+    public void addFirst(int data){
+        ListNode newNode = new ListNode(data);
+        if(isEmpty()){  //we can do (head==null) but we need to show the usage of isEmpty.
+            tail = head = newNode;
+        }else {
+            head.previous = newNode;
+            newNode.next = head;
+            head = newNode;
+        }
+        size++;
+    }
     class ListNode{
         int data;
         ListNode previous, next;
