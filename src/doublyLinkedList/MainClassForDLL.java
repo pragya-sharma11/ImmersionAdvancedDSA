16b5995ab1d0e7b9ba37d3b11fa14d54c361acaf
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-08 11:31:43 +0530

    define main class to run DoublyLinkedList

---
+++ src/doublyLinkedList/MainClassForDLL.java
@@ -0,0 +1,10 @@
+package doublyLinkedList;
+
+public class MainClassForDLL {
+    public static void main(String[] args) {
+        DoublyLinkedList dll = new DoublyLinkedList();
+        dll.addFirst(56);
+        dll.addFirst(76);
+        dll.addFirst(34);
+    }
+}
