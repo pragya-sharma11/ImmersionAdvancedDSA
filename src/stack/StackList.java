c232ec713fd2e05d4b6a19f2ca33e031a7b6f8ee
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-15 11:23:03 +0530

    create listnode in StackList

---
+++ src/stack/StackList.java
@@ -0,0 +1,17 @@
+package stack;
+
+public class StackList {
+    private ListNode top;
+    private int length;
+    public int pop(){
+
+    }
+    class ListNode {
+        int data;
+        ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+        }
+    }
+}
