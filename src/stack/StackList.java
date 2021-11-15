ab03a3b7353bad5473b4cd6b6a43e463998f9163
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-15 11:26:56 +0530

    define push() for StackList

--- src/stack/StackList.java
+++ src/stack/StackList.java
@@ -5,12 +5,22 @@ import java.util.NoSuchElementException;
 public class StackList {
     private ListNode top;
     private int length;
+    public void push(int data){
+        length++;
+        if(top == null){
+            top = new ListNode(data);
+        }
+        ListNode newNode = new ListNode(data);
+        newNode.next = top;
+        top = newNode;
+    }
     public int pop(){
-        if(length<=0){
+        if(top==null){
             throw new NoSuchElementException("Stack is empty!!");
         }
         int deletedData = top.data;
         top = top.next;
+        length--;
         return deletedData;
     }
     class ListNode {
