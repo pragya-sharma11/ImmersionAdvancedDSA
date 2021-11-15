07584007d579ef78c159a836b98e92070fce2b4a
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-15 11:24:57 +0530

    define pop() for StackList

--- src/stack/StackList.java
+++ src/stack/StackList.java
@@ -1,10 +1,17 @@
 package stack;
 
+import java.util.NoSuchElementException;
+
 public class StackList {
     private ListNode top;
     private int length;
     public int pop(){
-
+        if(length<=0){
+            throw new NoSuchElementException("Stack is empty!!");
+        }
+        int deletedData = top.data;
+        top = top.next;
+        return deletedData;
     }
     class ListNode {
         int data;
