c1facc681e2ad54ff43b5e7bdf7c5ad6cd4bc381
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-15 11:39:03 +0530

    define reverseTheString() for ReverseOfString.java

--- src/stack/ReverseOfString.java
+++ src/stack/ReverseOfString.java
@@ -1,4 +1,17 @@
 package stack;
 
+import java.util.Stack;
+
 public class ReverseOfString {
+    public static String reverseTheString(String str) {
+        Stack<Character> stack = new Stack<>();
+        char[] chars = str.toCharArray();
+        for(char c:chars){
+            stack.push(c);
+        }
+        for (int i=0;i<str.length();i++){
+            chars[i]=(stack.pop());
+        }
+        return new String(chars);
+    }
 }
